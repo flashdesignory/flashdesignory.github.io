32dbc3cc614a2f9921743b49646ca91d66f0ea3e
Thorsten Kober <tkober@gmail.com>
2022-11-09 15:09:24 -0800

    Add files via upload

---
+++ business-card/business-card.js
@@ -0,0 +1,14 @@
+import sheet from "./business-card.css" assert { type: "css" };
+document.adoptedStyleSheets.push(sheet);
+
+export class BusinessCard {
+  constructor() {
+    this.container = document.createElement("div");
+    this.container.classList.add("business-card-container");
+    this.container.textContent = "Hello World 👋";
+  }
+
+  render() {
+    return this.container;
+  }
+}
